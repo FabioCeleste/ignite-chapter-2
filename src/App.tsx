31f6ff39882bfc6f66c334e3f2f0f509506d4069
Fabio <fabio.wow.lol@gmail.com>
2022-06-20 00:14:27 -0300

    componentizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
